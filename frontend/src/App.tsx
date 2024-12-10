5e6654bcfd963b8a7b6390ba71c222d9055d5955
baccrie <bakarerilwan1@gmail.com>
2024-12-10 06:24:29 +0100

    building the page layout

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav></nav>
+        <section></section>
+      </header>
+
+      <nav></nav>
+      <main>
+        <section className="orgs">Orgs</section>
+        <section className="Categories">Categories Link</section>
+      </main>
+
+      <footer>Footer</footer>
+    </>
+  );
+}
+
+export default App;
