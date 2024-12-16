bd47a2bb00b6692f887e0465d1eb3fffe67787e6
baccrie <bakarerilwan1@gmail.com>
2024-12-16 07:53:29 +0100

    Starting with the Organisation page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import Buisness from "./pages/Buisness";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<HomePage />} />
+          <Route path="org/:id" element={<Buisness />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -6,7 +6,7 @@ export default function Main() {
       <section className={styles.buisness}>
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/47b633c3-d776-40e6-b284-04d3fb1e3bf2.jpeg?im_w=1200&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
@@ -42,7 +42,7 @@ export default function Main() {
 
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/b48403df-2c64-40c4-a35f-13a275a0dcf1.jpeg?im_w=1200&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
@@ -60,7 +60,7 @@ export default function Main() {
 
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/44392d48-82f3-423c-983b-50111fd8d342.jpeg?im_w=720&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
@@ -78,7 +78,7 @@ export default function Main() {
 
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/5ce9960e-f0c6-442f-9cd4-d86192f0350f.jpeg?im_w=720&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
@@ -96,7 +96,7 @@ export default function Main() {
 
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/bac1d522-ccb8-4aef-bbc6-d89223305ec4.jpeg?im_w=1200&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
@@ -114,7 +114,7 @@ export default function Main() {
 
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/11558b29-3c6d-42cb-9ede-26c3142e0378.jpeg?im_w=1200&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
@@ -131,25 +131,7 @@ export default function Main() {
         </div>
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
-          </div>
-          <div className={styles.buisnessDetails}>
-            <div className={styles.buisnessSummary}>
-              <h3>Get it right Fitness</h3>
-              <span>250km away from you</span>
-              <p>Lagos, Nigeria</p>
-            </div>
-
-            <div className={styles.buisnessRating}>
-              <span>⭐</span>
-              <span className={styles.ratingValue}>4.8</span>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.buisnessCard}>
-          <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/67b41d11-8bc9-45dd-b633-5af5e51ab0ce.jpeg?im_w=720&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
@@ -167,7 +149,7 @@ export default function Main() {
 
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/0adc077e-73ab-4576-8389-54ccb1bbe8fd.jpeg?im_w=1200&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
@@ -185,7 +167,7 @@ export default function Main() {
 
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/9c30f2a0-e953-4550-a1d8-67ad0e9496aa.jpeg?im_w=1200&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
@@ -203,7 +185,25 @@ export default function Main() {
 
         <div className={styles.buisnessCard}>
           <div className={styles.imgContainer}>
-            <img src="/test.webp" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/a25b469e-5145-435f-a3c5-9da1fe50cf9d.jpeg?im_w=1200&im_format=avif" />
+          </div>
+          <div className={styles.buisnessDetails}>
+            <div className={styles.buisnessSummary}>
+              <h3>Get it right Fitness</h3>
+              <span>250km away from you</span>
+              <p>Lagos, Nigeria</p>
+            </div>
+
+            <div className={styles.buisnessRating}>
+              <span>⭐</span>
+              <span className={styles.ratingValue}>4.8</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.buisnessCard}>
+          <div className={styles.imgContainer}>
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/361ed694-e8cb-4a27-804e-31b2537abe15.jpeg?im_w=1200&im_format=avif" />
           </div>
           <div className={styles.buisnessDetails}>
             <div className={styles.buisnessSummary}>
