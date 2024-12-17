ebe07909077af4747731be42141ee8abcf2c6152
baccrie <bakarerilwan1@gmail.com>
2024-12-17 08:32:27 +0100

    Styling the org section -- frontend

--- frontend/src/pages/Buisness.tsx
+++ frontend/src/pages/Buisness.tsx
@@ -29,7 +29,7 @@ export default function () {
 
         <section className={styles.gallery}>
           <div className={styles.mainImage}>
-            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/361ed694-e8cb-4a27-804e-31b2537abe15.jpeg?im_w=1200&im_format=avif" />
+            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/9c30f2a0-e953-4550-a1d8-67ad0e9496aa.jpeg?im_w=1200&im_format=avif" />
           </div>
 
           <div className={styles.otherImages}>
@@ -37,7 +37,7 @@ export default function () {
               <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/a25b469e-5145-435f-a3c5-9da1fe50cf9d.jpeg?im_w=1200&im_format=avif" />
             </div>
             <div className={styles.photo}>
-              <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/9c30f2a0-e953-4550-a1d8-67ad0e9496aa.jpeg?im_w=1200&im_format=avif" />
+              <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/361ed694-e8cb-4a27-804e-31b2537abe15.jpeg?im_w=1200&im_format=avif" />
             </div>
             <div className={styles.photo}>
               <img src="https://a0.muscache.com/im/pictures/miso/Hosting-977994569481876996/original/0adc077e-73ab-4576-8389-54ccb1bbe8fd.jpeg?im_w=1200&im_format=avif" />
@@ -59,14 +59,17 @@ export default function () {
               </header>
 
               <div className={styles.reviewSummary}>
-                <span className={styles.title}>
+                <div className={styles.title}>
                   <img src={FavouriteLeft} />
-                  Guest
+                  <div className={styles.favourite}>
+                    <span>Guest</span>
+                    <span>Favourite</span>
+                  </div>
                   <img src={FavouriteRight} />
-                </span>
-                <span className={styles.summary}>
+                </div>
+                <div className={styles.summary}>
                   One of the most love places on near according to guest
-                </span>
+                </div>
                 <div className={styles.ratings}>
                   <span>4.96</span>
                   <div className={styles.stars}>
