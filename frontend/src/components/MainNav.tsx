361c6b2fecea7ec0ebe6ae5b741c27642e9cd226
baccrie <bakarerilwan1@gmail.com>
2024-12-19 23:27:07 +0100

    Adjusting the UI to fit in

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,7 +1,19 @@
-import World from "../assets/world.svg";
+import { NavLink } from "react-router";
+
+import Kitchen from "../assets/kitchen.svg";
 import styles from "./MainNav.module.css";
 import Next from "../assets/next.svg";
 import Prev from "../assets/back.svg";
+import Star from "../assets/star.svg";
+import Park from "../assets/park.svg";
+import Outdoor from "../assets/outdoor.svg";
+import Ocean from "../assets/ocean.svg";
+import Sea from "../assets/sea.svg";
+import Beach from "../assets/beach.svg";
+import Wifi from "../assets/wifi.svg";
+import Tv from "../assets/tv.svg";
+import Camera from "../assets/cameras.svg";
+import Alarm from "../assets/alarm.svg";
 
 import { useRef } from "react";
 
@@ -10,7 +22,7 @@ export default function MainNav() {
 
   const scrollExtent = 200;
 
-  const handleScroll = (direction) => {
+  const handleScroll = (direction: string) => {
     console.log("works");
     if (navEl.current) {
       const scrollValue = direction === "left" ? -scrollExtent : scrollExtent;
@@ -22,86 +34,131 @@ export default function MainNav() {
     <nav className={styles.mainNav}>
       <ul ref={navEl}>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Kitchen} alt="Kitchen" />
+            <span>Kitchen</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Star} alt="Beachfront" />
+            <span>Beachfront</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Park} alt="Park" />
+            <span>Park</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Outdoor} alt="Tourist" />
+            <span>Tourist</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Ocean} alt="Club" />
+            <span>Club</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Sea} alt="Tech" />
+            <span>Tech</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Beach} alt="Electronics" />
+            <span>Electronics</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Wifi} alt="Cafe" />
+            <span>Cafe</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Tv} alt="Cinema" />
+            <span>Cinema</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Ocean} alt="Hotel" />
+            <span>Hotel</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Park} alt="Restaurant" />
+            <span>Restaurant</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Camera} alt="Spa" />
+            <span>Spa</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Alarm} alt="Sport" />
+            <span>Sport</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Kitchen} alt="Pet" />
+            <span>Pet</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Star} alt="Finances" />
+            <span>Finances</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Park} alt="Transport" />
+            <span>Transport</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Outdoor} alt="Education" />
+            <span>Education</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Ocean} alt="Fitness" />
+            <span>Fitness</span>
+          </NavLink>
         </li>
         <li>
-          <img src={World} />
-          <span>Beachfront</span>
+          <NavLink to="#">
+            <img src={Sea} alt="Beachfront" />
+            <span>Beachfront</span>
+          </NavLink>
         </li>
+        <li>
+          <NavLink to="#">
+            <img src={Beach} alt="Beachfront" />
+            <span>Beachfront</span>
+          </NavLink>
+        </li>
+
         <button
           className={styles.nextBtn}
           onClick={() => handleScroll("right")}
         >
-          <img src={Next} />
+          <img src={Next} alt="next" />
         </button>
         <button className={styles.prevBtn} onClick={() => handleScroll("left")}>
           <img src={Prev} alt="prev" />
@@ -110,12 +167,12 @@ export default function MainNav() {
 
       <div className={styles.filterBox}>
         <div className={styles.filters}>
-          <img src={World} />
+          <img src={Kitchen} />
           <span>Filters</span>
         </div>
 
         <div className={styles.toggle}>
-          <span>Display total before taxes</span>
+          <span>Display locale with location</span>
           <div className={styles.toggleContainer}>
             <div className="circle">&nbsp;</div>
           </div>
